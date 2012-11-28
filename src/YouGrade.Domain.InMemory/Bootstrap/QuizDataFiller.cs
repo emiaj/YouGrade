ff07ab6f17dcf4bd5ad975bd50a0cfec7af20284
jfebres <jaime.febres@gmail.com>
2012-11-28 01:11:15 -0500

    Added one more question.

--- src/YouGrade.Domain.InMemory/Bootstrap/QuizDataFiller.cs
+++ src/YouGrade.Domain.InMemory/Bootstrap/QuizDataFiller.cs
@@ -17,7 +17,7 @@ namespace YouGrade.Domain.InMemory.Bootstrap
         public void Activate(IEnumerable<IPackageInfo> packages, IPackageLog log)
         {
             var quiz = new Quiz(1, "Learning English with \"The Big Bang Theory\"", "Show your knowledge on the English language using videos from The Big Bang Theory", "en");
-            
+
             var question1 = new Question(1,
                 "In this episode, Sheldon proposes to Raj a variation for the classic \"Jokempo\" game: instead of \"rock-paper-scissors\", the variation is \"rock-paper-scissors-lizard-spock\". In his explanation for the game rules, Sheldon points out that paper beats Spock. How can Spock be defeated by paper?", "", "iapcKVn7DdY");
 
@@ -56,13 +56,12 @@ namespace YouGrade.Domain.InMemory.Bootstrap
                                                          "Because the time machine was damaged.",
                                                          false));
 
-
             var question3 = new Question(3,
                 "At the University cafeteria, Sheldon is discussing with his friend Leonard the problem with teleportation. At the end, Leonard seems to agree with Sheldon that there is a problem. What problem they are talking about?", "", "PQZzSrAIp-E");
 
 
             question3.Alternatives.Add(new Alternative(1,
-                                                         "Both agreed that it would be a problem if the teletransporter had to disintegrate the original Sheldon in order to create the new Sheldon",
+                                                         "Both agreed that it would be a problem if the teletransporter had to disintegrate the original Sheldon in order to create the new Sheldon.",
                                                          false));
             question3.Alternatives.Add(new Alternative(2,
                                                          "Both agreed that it would be a problem if the new Sheldon created by the teletransporter had no new improvements compared to the old Sheldon.",
@@ -77,9 +76,30 @@ namespace YouGrade.Domain.InMemory.Bootstrap
                                                          true));
 
 
+            var question4 = new Question(4,
+                                         "In this video, somebody is auctioning a time machine replica from the \"Time Machine\" movie. Leonard bid $800, and when the auction ends, Sheldon is surprised that no one else bid, since it's a piece of sci-fi movie memorabilia. In the end of the video, Sheldon says he understood why no-one else bid. Why he said so?",
+                                         "", "fRaUVp5DfRk");
+
+
+            question4.Alternatives.Add(new Alternative(1,
+                                                         "Because instead of a miniature, Sheldon found the time machine replica to be in fact full-sized and extravagant.",
+                                                         true));
+            question4.Alternatives.Add(new Alternative(2,
+                                                         "Because Sheldon saw it wasn''t a real time machine.",
+                                                         false));
+
+            question4.Alternatives.Add(new Alternative(3,
+                                                         "Because it was nothing like the time machine in the movie.",
+                                                         false));
+
+            question4.Alternatives.Add(new Alternative(4,
+                                                       "Because the time machine was damaged.",
+                                                       true));
+
             quiz.Questions.Add(question1);
             quiz.Questions.Add(question2);
             quiz.Questions.Add(question3);
+            quiz.Questions.Add(question4);
 
             _service.Add(quiz);
         }
